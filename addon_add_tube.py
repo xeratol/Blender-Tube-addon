973ee84fbfd902b0c436a05a1a069d093e3fdeda
Francis Joseph Serina <francis.serina@gmail.com>
2021-08-16 20:24:11 +0800

    Created addon_add_tube.py

---
+++ addon_add_tube.py
@@ -0,0 +1,181 @@
+# ##### BEGIN GPL LICENSE BLOCK #####
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software Foundation,
+#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# ##### END GPL LICENSE BLOCK #####
+
+bl_info = {
+    "name": "Tube",
+    "author": "Francis Joseph Serina",
+    "version": (0, 0, 1),
+    "blender": (2, 80, 0),
+    "location": "View3D > Add > Mesh",
+    "description": "Adds a new Tube",
+    "warning": "",
+    "doc_url": "",
+    "category": "Add Mesh",
+}
+
+
+import bpy
+from bpy.types import Operator
+from bpy.props import IntProperty, FloatProperty
+from bpy_extras.object_utils import AddObjectHelper, object_data_add
+import math
+
+def polar_coords(radius, angleRad, z = 0):
+    vert = (radius * math.cos(angleRad), radius * math.sin(angleRad), z)
+    return vert
+
+def flip_face(face):
+    newFace = face[::-1]
+    return newFace
+
+def flip_faces(faces):
+    newFaces = []
+    for f in faces:
+        newFaces.append(flip_face(f))
+    return newFaces
+
+def create_arc(radius, numSegments, z, arc):
+    angleRad = math.radians( arc / numSegments)
+    verts = [polar_coords(radius, angleRad * i, z) for i in range(numSegments) ]
+    return verts
+
+def bridge_loops(numVerts, startIdxUpper, startIdxLower):
+    faces = []
+    for i in range(numVerts - 1):
+        face = (i + startIdxUpper + 1, i + startIdxUpper, i + startIdxLower, i + startIdxLower + 1)
+        faces.append(face)
+    face = (startIdxUpper, startIdxUpper + numVerts - 1, startIdxLower + numVerts - 1, startIdxLower)
+    faces.append(face)
+    return faces
+
+def add_object(self, context):
+    if self.radius1 <= self.radius2:
+        innerRadius = self.radius1
+        outerRadius = self.radius2
+    elif self.radius1 > self.radius2:
+        innerRadius = self.radius2
+        outerRadius = self.radius1
+
+    verts = []
+    startIdxInnerUpper = len(verts)
+    verts.extend( create_arc( innerRadius, self.vertices, self.width / 2, self.arc ) )
+    startIdxOuterUpper = len(verts)
+    verts.extend( create_arc( outerRadius, self.vertices, self.width / 2, self.arc ) )
+    startIdxInnerLower = len(verts)
+    verts.extend( create_arc( innerRadius, self.vertices, -self.width / 2, self.arc ) )
+    startIdxOuterLower = len(verts)
+    verts.extend( create_arc( outerRadius, self.vertices, -self.width / 2, self.arc ) )
+
+    faces = []
+    faces.extend( bridge_loops( self.vertices, startIdxInnerLower, startIdxInnerUpper ) )
+    faces.extend( bridge_loops( self.vertices, startIdxOuterUpper, startIdxOuterLower ) )
+    faces.extend( bridge_loops( self.vertices, startIdxInnerUpper, startIdxOuterUpper ) )
+    faces.extend( bridge_loops( self.vertices, startIdxOuterLower, startIdxInnerLower ) )
+    
+    mesh = bpy.data.meshes.new(name="Tube")
+    mesh.from_pydata(verts, [], faces)
+    # useful for development when the mesh may be invalid.
+    mesh.validate(verbose=True)
+    object_data_add(context, mesh, operator=self)
+
+
+class AddTube(Operator, AddObjectHelper):
+    """Create a new Tube"""
+    bl_idname = "mesh.primitive_tube"
+    bl_label = "Add Tube"
+    bl_options = {'REGISTER', 'UNDO', 'PRESET'}
+
+    vertices: IntProperty(
+        name="Vertices",
+        description="Number of segments",
+        default=32,
+        min=3,
+        soft_max=1000,
+    )
+    
+    radius1: FloatProperty(name="Radius 1",
+        description="Radius 1 of the tube",
+        min=0.0,
+        soft_max=1000.0,
+        unit='LENGTH',
+        default=4.0
+    )
+
+    radius2: FloatProperty(name="Radius 2",
+        description="Radius 2 of the tube",
+        min=0.0,
+        soft_max=1000.0,
+        unit='LENGTH',
+        default=5.0
+    )
+
+    arc: FloatProperty(name="Arc",
+        description="Portion of the circumference",
+        min=0.0,
+        max=360.0,
+        default=360.0
+    )
+
+    width: FloatProperty(name="Width",
+        description="Thickness of tube",
+        min=0.0,
+        soft_max=1000.0,
+        unit='LENGTH',
+        default=0.2
+    )
+
+    def draw(self, context):
+        layout = self.layout
+
+        box = layout.box()
+        box.prop(self, 'vertices')
+        box.prop(self, 'radius1')
+        box.prop(self, 'radius2')
+        box.prop(self, 'arc')
+        box.prop(self, 'width')
+
+        box = layout.box()
+        box.prop(self, 'align', expand=True)
+        box.prop(self, 'location', expand=True)
+        box.prop(self, 'rotation', expand=True)
+            
+    def execute(self, context):
+
+        add_object(self, context)
+
+        return {'FINISHED'}
+
+# Registration
+
+def add_object_button(self, context):
+    self.layout.operator(
+        AddTube.bl_idname,
+        text="Tube",
+        icon='MESH_CYLINDER')
+
+def register():
+    bpy.utils.register_class(AddTube)
+    bpy.types.VIEW3D_MT_mesh_add.append(add_object_button)
+
+def unregister():
+    bpy.utils.unregister_class(AddTube)
+    bpy.types.VIEW3D_MT_mesh_add.remove(add_object_button)
+
+
+if __name__ == "__main__":
+    register()
